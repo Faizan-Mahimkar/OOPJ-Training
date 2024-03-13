52e1a6b760f3534cab878ce11cbb0d05e6007204
Faizan Mahimkar <112464549+Faizan-Mahimkar@users.noreply.github.com>
2024-03-13 11:45:43 +0530

    Add files via upload

---
+++ MorningSession/ThreadEg.java
@@ -0,0 +1,17 @@
+class MyThread extends Thread{
+    public void run(){
+        for(int i=1;i<=10;++i) {
+            System.out.println("Child Thread");
+        }
+    }
+}
+
+public class ThreadEg {
+    public static void main(String[] args) {
+        MyThread m = new MyThread();
+        m.start();
+        for(int i=1;i<=10;++i) {
+            System.out.println("Main Thread");
+        }
+    }
+}
